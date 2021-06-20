8b6683b051ce926abae34a67b930c17358c58962
alesia-k <aliciaJC@yandex.ru>
2021-06-20 12:10:30 +0200

    js for burger-menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,27 +6,3 @@ function toggleMenu() {
 }
 
 btnEl.onclick = toggleMenu;
-
-/*function logMenuClick() {
-    console.log('menu button has clicked');
-}
-
-btnEl.addEventListener('click', toggleMenu);
-btnEl.addEventListener('dblclick', logMenuClick);
-
-var body = document.body;
-
-body.addEventListener('click', function(event) {
-    var isTargetBtn = false;
-    
-    for (var i = 0; i < btnEl.children.length; i ++) {
-        if (btnEl.children[i] == event.target) {
-            isTargetBtn = true;
-            return;
-        }
-    }
-    
-    if (!isTargetBtn) {
-        menuEl.classList.remove('open');        
-    }
-});*/
